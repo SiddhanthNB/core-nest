a4f0420b9a658220e136f6539f9e93a08c8c4e9a
Siddhanth Bhimakari <siddhanth.n.b@gmail.com>
2025-04-05 16:02:38 +0100

    changed summarization model

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -20,7 +20,7 @@ logger.debug("Starting to load models...")
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 logger.debug("SentenceTransformer model loaded successfully.")
 
-summarizer_model = pipeline("summarization", model="Falconsai/text_summarization")
+summarizer_model = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 logger.debug("Summarization model loaded successfully.")
 
 logger.debug("Model loading completed.")
